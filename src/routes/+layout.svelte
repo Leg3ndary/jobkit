b783cc48474ecabb9369c1deabe785477e583690
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-05-21 20:15:23 -0400

    Start making everything look ince

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,11 @@
-<slot />
 <script>
     import "../app.css";
 </script>
+
+<nav class="flex gap-5 text-white bg-black py-2 px-4 text-xl items-center font-medium">
+    <a href="/" class="text-yellow-400 font-black text-2xl mr-auto">JobKit</a>
+    <a href="/about">About</a>
+    <a href="/Pricing">Pricing</a>
+    <a href="/Dashboard">Dashboard</a>
+</nav>
+<slot />
