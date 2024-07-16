0e0d3f0a3513375564bfad06b5c649274bb2e8a5
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-07-16 17:44:20 -0400

    Start changing initial design

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-
-
 export default function Home() {
     return (
-        <main className="flex flex-col items-center justify-center min-h-screen py-40 bg-custom-hsla bg-custom-radial2">
-            <div className="flex flex-col items-center justify-center font-sans text-3xl">
-                <h1 className="mb-8 text-6xl">
-                    <span className="text-blue-500">Job</span>kit
-                </h1>
-                <div className="p-4 my-8 text-xl">Scroll down to discover</div>
-                <div className="w-1 h-64 bg-black"></div>
+        <main className="flex flex-col items-center justify-center min-h-screen">
+            <div className="flex flex-col items-center justify-center h-full w-full">
+                <div className="inset-0 min-h-screen flex items-center justify-center flex-col w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] ">
+                    <h1 className="mb-8 text-7xl font-black">
+                        <span className="text-blue-500">Job</span>kit
+                    </h1>
+                    <div className="p-4 my-8 text-3xl">
+                        Scroll down to discover
+                    </div>
+                </div>
             </div>
         </main>
     );
 }
-
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,29 +8,7 @@ const config: Config = {
     ],
     theme: {
         extend: {
-            backgroundColor: {
-                "custom-hsla": "hsla(0,100%,50%,1)",
-            },
-            backgroundImage: {
-                "custom-radial": `
-                    radial-gradient(at 40% 20%, hsla(28,100%,74%,1) 0px, transparent 50%),
-                    radial-gradient(at 80% 0%, hsla(189,100%,56%,1) 0px, transparent 50%),
-                    radial-gradient(at 0% 50%, hsla(355,100%,93%,1) 0px, transparent 50%),
-                    radial-gradient(at 80% 50%, hsla(340,100%,76%,1) 0px, transparent 50%),
-                    radial-gradient(at 0% 100%, hsla(22,100%,77%,1) 0px, transparent 50%),
-                    radial-gradient(at 80% 100%, hsla(242,100%,70%,1) 0px, transparent 50%),
-                    radial-gradient(at 0% 0%, hsla(343,100%,76%,1) 0px, transparent 50%)
-                `,
-                "custom-radial2": `
-                    radial-gradient(at 27% 11%, hsla(320,61%,71%,1) 0px, transparent 50%),
-                    radial-gradient(at 60% 88%, hsla(216,89%,61%,1) 0px, transparent 50%),
-                    radial-gradient(at 4% 67%, hsla(292,73%,79%,1) 0px, transparent 50%),
-                    radial-gradient(at 69% 45%, hsla(329,93%,61%,1) 0px, transparent 50%),
-                    radial-gradient(at 88% 12%, hsla(314,90%,66%,1) 0px, transparent 50%),
-                    radial-gradient(at 73% 63%, hsla(89,62%,74%,1) 0px, transparent 50%),
-                    radial-gradient(at 9% 83%, hsla(143,68%,68%,1) 0px, transparent 50%);
-                `,
-            },
+            
         },
     },
     plugins: [],
