4fda8aa9b6a63cd4ab4b0b24bd81437adce8c09c
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-07-17 17:59:37 -0400

    Update main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 export default function Home() {
     return (
         <main className="flex flex-col items-center justify-center min-h-screen">
-            <div className="flex flex-col items-center justify-center h-full w-full">
-                <div className="inset-0 min-h-screen flex items-center justify-center flex-col w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] ">
-                    <h1 className="mb-8 text-7xl font-black">
-                        <span className="text-blue-500">Job</span>kit
-                    </h1>
-                    <div className="p-4 my-8 text-3xl">
-                        Scroll down to discover
-                    </div>
-                </div>
-            </div>
+            <section className="inset-0 min-h-screen flex items-center justify-center flex-col w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] ">
+                <h1 className="mb-8 text-7xl font-black">
+                    <span className="text-blue-500">Job</span>kit
+                </h1>
+                <div className="p-4 my-8 text-3xl">Scroll down to discover</div>
+            </section>
+            <section className="min-h-screen w-full">
+            
+            </section>
         </main>
     );
 }
